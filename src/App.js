32b57a32583f34686d2396011e40a4655844a919
Tima19 <temirlan.baltabaev6@gmail.com>
2023-10-11 19:48:16 +0600

    lesson1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ContactsPage from './pages/contactsPage/ContactsPage';
+import AboutPage from './pages/aboutPage/AboutPage';
+import MainPage from './pages/mainPage/MainPage';
+
+function App() {
+  return (
+    <div className="App">
+      <ContactsPage />
+    </div>
+  );
+}
+
+export default App;
